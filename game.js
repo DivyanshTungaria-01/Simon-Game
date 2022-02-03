9fe9acac453ace7320c22000f04beebf73c3213c
DivyanshTungaria-01 <divyanshtungaria@gmail.com>
2022-02-04 01:11:02 +0530

    random numbers generated

---
+++ game.js
@@ -0,0 +1,39 @@
+
+var gamePattern = [];
+
+var buttonColours = ["red","blue","green","yellow"];
+
+function nextSequence(){
+
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+
+    // function nextSequence(){                                          We can also use this instead of above statements.
+//     var randomNumber = Math.floor(Math.random()*4);
+    
+//     var randomChosenColour;
+//     if(randomNumber === 0){
+//         randomChosenColour = "green";
+//         gamePattern.push(randomChosenColour);
+//     }
+//     else if(randomNumber === 1){ 
+//         randomChosenColour = "red";
+//         gamePattern.push(randomChosenColour);
+//     }
+//     else if(randomNumber === 2){
+//         randomChosenColour = "yellow";
+//         gamePattern.push(randomChosenColour);
+//     }
+//     else if(randomNumber === 3){
+//         randomChosenColour = "blue";
+//         gamePattern.push(randomChosenColour);
+//     }
+
+}
+
+
+
+
+
