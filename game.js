27026fb0458b261448c5ca1ac9e17436c80913e7
DivyanshTungaria-01 <divyanshtungaria@gmail.com>
2022-02-07 02:45:42 +0530

    sound and animation added

--- game.js
+++ game.js
@@ -1,30 +1,57 @@
 
-
 var buttonColours = ["red","blue","green","yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+    // if(level === 0){
+    //     nextSequence();
+    // }
+    if(!started){
+        $("h1").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
 
 $(".btn").click(function(){
 
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
-
 function nextSequence(){
+
+    level++;
+    $("h1").text("Level" + " " + level);
 
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);   
 
-   
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
+function animatePress(currentColour){
 
-nextSequence();
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    },100);
+}
 
 
 
